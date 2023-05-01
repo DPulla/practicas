81593763b8458a89a35a360cdd94ed595a718178
DPulla <dardo95na@gmail.com>
2023-05-01 02:00:37 -0500

    Se agrega semáforo

--- prac3_semaforos.c
+++ prac3_semaforos.c
@@ -8,6 +8,8 @@
 
 #define ITER 100000
 
+sem_t sem1;
+
 static RT_TASK tarea1;
 static RT_TASK tarea2;
 
@@ -15,17 +17,22 @@ int global = 0;
 
 void tareaUno(void *arg){
     for(int i=0; i < ITER; i++){
+        sem__wait(&sem1);
         printf("Tarea 1 la variable global es %d -----\n", ++global);
+        sem__post(&sem1);
     }
 }
 
 void tareaDos(void *arg){
     for(int i=0; i < ITER; i++){
+        sem__wait(&sem1);
         printf("Tarea 2 la variable global es %d -----\n", --global);
+        sem__post(&sem1);
     }
 }
 
 int main(int argc, char* argv[]){
+    sem__init(&sem1, 0, 1);
     rt_task_create(&tarea1, "tarea 1", 0 , 1, 0);
     rt_task_create(&tarea2, "tarea 2", 0 , 1, 0);
     rt_task_start(&tarea1, &tareaUno,0);
