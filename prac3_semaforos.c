f1f560b4e6ace29827370528ee0d19cf07539b66
DPulla <dardo95na@gmail.com>
2023-05-01 03:26:51 -0500

    S

--- prac3_semaforos.c
+++ prac3_semaforos.c
@@ -17,7 +17,7 @@ int global = 0;
 void tareaUno(void *arg){
     for(int i=0; i < ITER; i++){
         rt_sem_v(&sem);
-        printf("Tarea 1 la variable global es %d -----\n", ++global);
+        printf("Tarea 1 la variable global es %d -----\n", ++global, "en la iteración... %d", ITER);
         rt_sem_p(&sem, TM_INFINITE);
     }
 }
@@ -25,7 +25,7 @@ void tareaUno(void *arg){
 void tareaDos(void *arg){
     for(int i=0; i < ITER; i++){
         rt_sem_v(&sem);
-        printf("Tarea 2 la variable global es %d -----\n", --global);
+        printf("Tarea 2 la variable global es %d -----\n", --global, "en la iteración... %d", ITER);
         rt_sem_p(&sem, TM_INFINITE);
     }
 }
